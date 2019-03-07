362dcd0e2cd9044e9df3670586dcc1d7c3e8a06c
Joy Lin <vvlnote@hotmail.com>
2019-03-07 04:17:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,42 @@
 def consolidate_cart(cart)
   # code here
+  consolidated_cart = {}
+  cart.each do |item|
+    if consolidated_cart.has_key?(item.keys[0])
+      consolidated_cart[item.keys[0]][:count] += 1
+    else
+      consolidated_cart[item.keys[0]] = item.values[0]
+      consolidated_cart[item.keys[0]][:count] = 1
+    end
+  end
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |coupon|
+    if cart.has_key?(coupon[:item])
+      count = 0
+      while cart[coupon[:item]][:count] >= coupon[:num]
+        cart[coupon[:item]][:count] -= coupon[:num]
+        count += 1
+      end
+      if count > 0
+        new_key = coupon[:item] + " W/COUPON"
+        new_attribute = {}
+        new_attribute[:price] = coupon[:cost]
+        new_attribute[:clearance] = cart[coupon[:item]][:clearance]
+        new_attribute[:count] = count
+        cart[new_key] = new_attribute
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
   # code here
+  
 end
 
 def checkout(cart, coupons)
