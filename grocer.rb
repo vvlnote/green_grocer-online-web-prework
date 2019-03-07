93813155072dba67a590ed238ab02f837185019b
Joy Lin <vvlnote@hotmail.com>
2019-03-07 04:43:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -51,7 +51,9 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   puts "after consolidated - #{consolidated_cart}"
   consolicated_cart = apply_coupons(consolidated_cart, coupons)
+  puts "after coupons - #{consolidated_cart}"
   consolidated_cart = apply_clearance(consolidated_cart)
+  puts "after clearance -#{consolidated_cart}"
   item_name = ""
   for i in 0...consolidated_cart.length
     item_name = consolidated_cart.keys[i]
